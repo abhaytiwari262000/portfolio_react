74d06b0a79e41e6a4584100bb07af85ef97729ed
abhaytiwari262000 <abhaytiwari262000@gmail.com>
2024-07-03 15:36:17 +0530

    c

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:abhaytiwari262000@gmail.com">myemail@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/abhay-tiwari-08643019b/">linkedin.com/myname</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/abhaytiwari262000">github.com/myname</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ src/components/Navbar/test.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const test = () => {
+  return (
+    <div style={{backgroundColor:'red'}}>
+      <div style={{backgroundColor:'blue'}}>
+        1
+      </div>
+      <div style={{backgroundColor:'green'}}>
+        2
+      </div>
+    </div>
+  )
+}
+
+export default test
